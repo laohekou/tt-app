aa2922af4fde2b2dfb9adcc95f3df362d1c111e9
xiongy <15810265860@163.com>
2022-03-28 15:57:02 +0800

    添加

--- src/Support/helper.php
+++ src/Support/helper.php
@@ -1,2 +1,15 @@
 <?php
 
+if (!function_exists('get_client_ip')) {
+    function get_client_ip()
+    {
+        if (!empty($_SERVER['REMOTE_ADDR'])) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        } else {
+            // for php-cli(phpunit etc.)
+            $ip = defined('PHPUNIT_RUNNING') ? '127.0.0.1' : gethostbyname(gethostname());
+        }
+
+        return filter_var($ip, FILTER_VALIDATE_IP) ?: '127.0.0.1';
+    }
+}
