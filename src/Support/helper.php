718ee143ad6720f3c652cc2c6e148c3f74fc18d5
xiongy <15810265860@163.com>
2022-03-30 10:44:30 +0800

    func添加

--- src/Support/helper.php
+++ src/Support/helper.php
@@ -13,3 +13,17 @@ if (!function_exists('get_client_ip')) {
         return filter_var($ip, FILTER_VALIDATE_IP) ?: '127.0.0.1';
     }
 }
+
+if (!function_exists('tt_app')) {
+    function tt_app(array $config)
+    {
+        return new \Xyu\TtApp\TtApp($config);
+    }
+}
+
+if (!function_exists('ttapp')) {
+    function ttapp(string $name = 'default')
+    {
+        return \Hyperf\Utils\ApplicationContext::getContainer()->get(\Xyu\TtApp\Hyperf\Factory::class)->make($name);
+    }
+}
