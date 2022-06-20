75d034027833bd6d43448f5b74ff1c88f161d6fb
xiongy <15810265860@163.com>
2022-06-20 20:33:01 +0800

    如果token原因，重试业务接口 1

--- src/Contract/AbstractGateway.php
+++ src/Contract/AbstractGateway.php
@@ -40,23 +40,23 @@ abstract class AbstractGateway
         } elseif ($errorCode == 10008 || $errorCode == 2190008)
         {
             $resp = $this->app->douyin_token->setRefreshToken($this->app->douyin_token->getToken()['refresh_token'])->getToken();
-            if(isset($resp['error_code']) && $resp['error_code'] == 10010)
+            if(isset($resp['data']['error_code']) && $resp['data']['error_code'] == 10010)
             {
                 $result = $this->app->douyin_token->renewRefreshToken($this->app->douyin_token->getToken()['refresh_token']);
-                if(isset($result['error_code']) && $result['error_code'] == 0) {
+                if(isset($result['data']['error_code']) && $result['data']['error_code'] == 0) {
                     $res = $this->app->douyin_token->setRefreshToken($result['data']['refresh_token'])->getToken();
-                    if(isset($res['error_code']) && $res['error_code'] == 0) {
+                    if(isset($res['data']['error_code']) && $res['data']['error_code'] == 0) {
                         return true;
                     }else{
-                        throw new DyTokenException('刷新refresh_token后获取token失败，重新走用户授权流程', $res['error_code']);
+                        throw new DyTokenException('刷新refresh_token后获取token失败，重新走用户授权流程', $res['data']['error_code'] ?? 8888);
                     }
                 }else{
-                    throw new DyTokenException('刷新refresh_token失败，重新走用户授权流程', $result['error_code']);
+                    throw new DyTokenException('刷新refresh_token失败，重新走用户授权流程', $result['data']['error_code'] ?? 7777);
                 }
-            } elseif (isset($resp['error_code']) && $resp['error_code'] == 0) {
+            } elseif (isset($resp['data']['error_code']) && $resp['data']['error_code'] == 0) {
                 return true;
             }else{
-                throw new DyTokenException('刷新access_token异常，重新走用户授权流程', $resp['error_code']);
+                throw new DyTokenException('刷新access_token异常，重新走用户授权流程', $resp['data']['error_code'] ?? 9999);
             }
         } else {
             // 非token状态码不处理
